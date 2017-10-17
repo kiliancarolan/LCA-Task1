05aa82dfd107d79fa74b75e50f87180652e063b0
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-10-17 12:21:24 +0100

    Added first Junit Test
    Junit test added checking for basic functionality of lowest common ancestor.

--- FirstAssingment/src/NodeTest.java
+++ FirstAssingment/src/NodeTest.java
@@ -5,8 +5,18 @@ import org.junit.Test;
 public class NodeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCorrectLCA() {
+		BinaryTree tree = new BinaryTree();
+        tree.root = new Node(20);
+        tree.root.left = new Node(8);
+        tree.root.right = new Node(22);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(12);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(14);
+        int n1 = 10, n2 = 14;
+        Node t = tree.lca(tree.root, n1, n2);
+        assertEquals("cheking for correct lowest ancestor",12,t.data);
 	}
 
 }
