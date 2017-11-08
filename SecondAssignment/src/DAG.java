168b9dc9525f868a648bee3cec51e62db521f1c0
kiliancarolan <32336995+kiliancarolan@users.noreply.github.com>
2017-11-08 14:48:44 +0000

    wrote methods
    Added an array for tracking nodes, a method for getting size of graph and to check if graph contains a node

--- SecondAssignment/src/DAG.java
+++ SecondAssignment/src/DAG.java
@@ -1,21 +1,42 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class DAG <Value> {
+public class DAG<Value> {
 
-		
-		public class Node{
-			private Value val;
-			private Node[] child;
-			private int childNumber;
-			
-			public Node(Value val, int childNumber){
-				this.val = val;
-				this.childNumber = childNumber;
-			}
-			int value;
+	public Node[] nodelist; // array to keep track of nodes
+
+	private class Node 
+	{
+		private Value val;
+		private Node[] child;
+		private int childNumber;
+
+		public Node(Value val, int childNumber) 
+		{
+			this.val = val;
+			this.childNumber = childNumber;
 		}
-		
-	
 
-		
+		int value;
+	}
+
+	// returns the size of graph
+	public int size() 
+	{
+		return nodelist.length;
+	}
+
+	// Checks to see if Node n is in the graph
+	public boolean contains(Node n) 
+	{
+		if (Arrays.asList(nodelist).contains(n) == true) 
+		{
+			return true;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+
 }
